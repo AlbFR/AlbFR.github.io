ddac319b21f2385363b1b1092a1c634eb07b3f96
AlbFR <68129256+AlbFR@users.noreply.github.com>
2021-02-24 09:22:09 -0300

    Add files via upload

---
+++ Note.js
@@ -0,0 +1,123 @@
+let notes = 
+{
+	//cusc = current scale
+	0:
+	{
+		nombre: "Do" ,
+		name: "C" ,
+		x: 0 ,
+		Dom: true ,
+		cusc: true ,
+		white: 0
+	} ,
+	1:
+	{
+		nombre: "Do#" ,
+		nombre1: "Reb" ,
+		name: "C#" ,
+		name1: "Db" ,
+		x: 31.25 ,
+		Dom: false ,
+		cusc: false ,
+		black: 0
+	} ,
+	2:
+	{
+		nombre: "Re" ,
+		name: "D" ,
+		x: 50 ,
+		Dom: true ,
+		cusc: true ,
+		white: 1
+	}
+	,
+	3:
+	{
+		nombre: "Re#" ,
+		nombre1: "Mib" ,
+		name: "D#" ,
+		name1: "Eb" ,
+		x: 50+31.25 ,
+		Dom: false ,
+		cusc: false ,
+		black: 1
+	},
+	4:
+	{
+		nombre: "Mi" ,
+		name: "E" ,
+		x: 100 ,
+		Dom: true ,
+		cusc: true ,
+		white: 2
+	},
+	5:
+	{
+		nombre: "Fa" ,
+		name: "F" ,
+		x: 150 ,
+		Dom: true ,
+		cusc: true ,
+		white: 3
+	},
+	6:
+	{
+		nombre: "Fa#" ,
+		nombre1: "Solb" ,
+		name: "F#" ,
+		name1: "Gb" ,
+		x: 150+31.25 ,
+		Dom: false ,
+		cusc: false ,
+		black: 2
+	},
+	7:
+	{
+		nombre: "Sol" ,
+		name: "G" ,
+		x: 200 ,
+		Dom: true ,
+		cusc: true ,
+		white: 4
+	},
+	8:
+	{
+		nombre: "Sol#" ,
+		nombre1: "Lab" ,
+		name: "G#" ,
+		name1: "Ab" ,
+		x: 200+31.25 ,
+		Dom: false ,
+		cusc: false ,
+		black: 3
+	},
+	9:
+	{
+		nombre: "La" ,
+		name: "A" ,
+		x: 250 ,
+		Dom: true ,
+		cusc: true ,
+		white: 5
+	},
+	10:
+	{
+		nombre: "La#" ,
+		nombre1: "Sib" ,
+		name: "A#" ,
+		name1: "Bb" ,
+		x: 250+31.25 ,
+		Dom: false ,
+		cusc: false ,
+		black: 4
+	},
+	11:
+	{
+		nombre: "Si" ,
+		name: "B" ,
+		x: 300 ,
+		Dom: true ,
+		cusc: true ,
+		white: 6
+	}
+}
